4006a2d30fe695b57bf16fbc15a0a74f3e9aaa76
Jose Rodriguez <isc.joserodriguez@gmail.com>
2022-08-27 10:12:51 -0600

    Servidor + Nodemon

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+/**
+ * 1.- Importamos Express
+ * 2.- Instanciamos una nueva aplicación
+ * // 3.- Configuración (Middlewares)
+ * 4.- Diseño de las rutas
+ * 5.- Levantar servidor
+ */
 
+const alumnos = ["Pedro", "Maria", "Jesus", "Luis"];
 
-console.log('Hola');
+//! 1.- Importamos Express
+const express = require("express");
+
+//! 2.- Instanciamos una nueva aplicación
+const app = express();
+
+//! 3.- Configuración (Middlewares)
+
+//! 4.- Diseño de las rutas
+app.get("/", (req, res) => {
+  console.log("Entraste a la petición");
+  res.send(alumnos);
+});
+
+//! 5.- Levantar servidor
+app.listen(3001, () => {
+  console.log("Servidor corriendo en el puerto 3001");
+});
